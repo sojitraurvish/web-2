4315700663f410ac619f84a098533b9b16767c1b
urvish sojitra <sojitraurvish0@gmail.com>
2025-10-09 15:21:03 +0530

    Enhanced WebSocket server functionality by adding build scripts, improved message handling, and room management. Introduced UUID for socket identification and refined connection logic. Updated package dependencies for user-ws.

--- 2 web sockets/p1 scaling websockets/relayer-ws/src/server.ts
+++ 2 web sockets/p1 scaling websockets/relayer-ws/src/server.ts
@@ -7,9 +7,12 @@ const server:WebSocket[] = []
 wss.on("connection",(ws:WebSocket)=>{
    
     server.push(ws);
+    console.log("Server connected", server.length);
 
     ws.on("message",(message:string)=>{
+        console.log("message received", message);
         server.forEach((ws:WebSocket)=>{
+            console.log("sending message to all servers", message);
             ws.send(message);
         })
     })
--- 2 web sockets/p1 scaling websockets/user-ws/src/server.ts
+++ 2 web sockets/p1 scaling websockets/user-ws/src/server.ts
@@ -1,42 +1,61 @@
 import {WebSocketServer, WebSocket} from "ws";
 import { WS_EVENTS} from "./utils/constants";
+import { v4 as uuidv4 } from 'uuid';
 
-const wss = new WebSocketServer({ port: 3000 });
+const wss = new WebSocketServer({ port: 3002 });
 
-interface Room{
-    sockets:WebSocket[];
-}
-
-const rooms:Record<string,Room>={};
+const rooms:Record<string,string[]>={};
+const sockets = new Map<string, WebSocket>();
 
 const RELAYER_URL = "ws://localhost:8000";
 const relayerSocket = new WebSocket(RELAYER_URL);
 
 relayerSocket.on("message",(message:string)=>{
-    wss.clients.forEach((client:WebSocket)=>{
-        client.send(message);
-    })
+
+           
+           const parsedMessage = JSON.parse(message);
+           const { room, socketId, type, message: msg } = parsedMessage;
+           
+           console.log("Parsed message from relayer:", parsedMessage);
+    const senderSocket = sockets.get(socketId);
+
+    console.log("Received message from relayer", sockets.keys());
+    
+    
+    if(rooms[room]){
+        rooms[room].forEach((socket)=>{
+            console.log("Socket", socket);
+            if(socket !== socketId){  // Exclude the sender
+                sockets.get(socket)?.send(JSON.stringify({ type: WS_EVENTS.MESSAGE, message: msg }));
+            }
+        });
+       
+    }else{
+        senderSocket?.send(JSON.stringify({ type: WS_EVENTS.ERROR, message: "Room not found" }));
+    }
 })
 
 // in websocket data comes in two formmrt only string and binary    
 wss.on("connection", (ws: WebSocket) => {
+    const socketId = uuidv4();
+    sockets.set(socketId, ws);
+    console.log("Socket connected", sockets.keys());
+    
     ws.on("error", (e)=>{
         console.log("Error", e);
     });
 
     ws.on("message", (message: string) => {
-        const { room, type, message: msg } = JSON.parse(message);
+        const { room, type, message: msg} = JSON.parse(message);
 
         if(type===WS_EVENTS.JOIN_ROOM){
             if(!rooms[room]){
-                rooms[room]={
-                    sockets:[]
-                };
+                rooms[room]=[]
             }
             
             // Check if the WebSocket is already in the room before adding
-            if(!rooms[room].sockets.includes(ws)){
-                rooms[room].sockets.push(ws);
+            if(!rooms[room].includes(socketId)){
+                rooms[room].push(socketId);
                 ws.send(JSON.stringify({ type: WS_EVENTS.CONNECTED, message: `User joined the room ${room} successfully!` }));
             } else {
                 ws.send(JSON.stringify({ type: WS_EVENTS.ERROR, message: `You are already in room ${room}` }));
@@ -44,19 +63,16 @@ wss.on("connection", (ws: WebSocket) => {
         }
 
         if(type===WS_EVENTS.SEND_MESSAGE){
-            console.log("Sending message to room", !rooms[room]);
-            if(rooms[room]){
-                if(rooms[room].sockets.includes(ws)){
-                    rooms[room].sockets.forEach((socket)=>{
-                        if(socket !== ws){  // Exclude the sender
-                            socket.send(JSON.stringify({ type: WS_EVENTS.MESSAGE, message: msg }));
-                        }
-                    });
-                }else{
-                    ws.send(JSON.stringify({ type: WS_EVENTS.ERROR, message: `You are not in room ${room}` }));
-                }
-            }else{
-                ws.send(JSON.stringify({ type: WS_EVENTS.ERROR, message: "Room not found" }));
+            if(rooms[room].includes(socketId)){
+                relayerSocket.send(JSON.stringify({
+                    type,
+                    room,
+                    message: msg,
+                    socketId,
+                }));
+            }
+            else{
+                ws?.send(JSON.stringify({ type: WS_EVENTS.ERROR, message: `You are not in room ${room}` }));
             }
         }
     });
@@ -64,11 +80,12 @@ wss.on("connection", (ws: WebSocket) => {
     // Clean up when WebSocket disconnects
     ws.on("close", () => {
         console.log("User disconnected");
+        sockets.delete(socketId);
         // Remove the socket from all rooms
         Object.keys(rooms).forEach((room) => {
-            rooms[room].sockets = rooms[room].sockets.filter((socket) => socket !== ws);
+            rooms[room] = rooms[room].filter((socket) => socket !== socketId);
             // Clean up empty rooms
-            if (rooms[room].sockets.length === 0) {
+            if (rooms[room].length === 0) {
                 delete rooms[room];
             }
         });
